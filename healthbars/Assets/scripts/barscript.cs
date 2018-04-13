526d271ab160b7bdf88826709807b5d566a177ca
BradleyBekker <30977961+BradleyBekker@users.noreply.github.com>
2018-04-13 11:23:59 +0200

    unity ui test

---
+++ healthbars/Assets/scripts/barscript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class barscript : MonoBehaviour {
+
+    private float fillContent;
+    public float Value = 100;
+        
+    
+
+    
+    [SerializeField]
+    private Image content;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+    private void Handlebar()
+    {
+
+
+        
+    }
+
+
+
+    private float Map(float value, float inMin,float inMax, float outMin, float outMax) { 
+    return (value -inMin)*(outMax - outMin) / (inMax-inMin) + outMin;
+    }
+
+
+    public void buttonadd()
+    {
+        if (Value != 100)
+        {
+            Value += 10;
+            content.fillAmount = Map(Value, 0, 100, 0, 1);
+        }
+    }
+    public void buttonremove()
+    {
+        if (Value != 0)
+        {
+            Value -= 10;
+            content.fillAmount = Map(Value, 0, 100, 0, 1);
+        }
+    }
+}
---
+++ healthbars/Assets/scripts/score.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class score : MonoBehaviour {
+
+    private int Score = 0;
+    public Text text;
+
+    // Use this for initialization
+    void Start () {
+        text.text = "score:" + Score.ToString();
+    }
+	
+    public void AddScore()
+    {
+        Score++ ;
+        text.text = "score:" + Score.ToString();
+    }
+    public void DeductScore()
+    {
+        if (Score > 0) {
+            Score--;
+            text.text = "score:" + Score.ToString();
+        }
+        }
+}
